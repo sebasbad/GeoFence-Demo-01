e38e214f4b9d660e2be56d3684d9de91041f0b0e
Sebastian Badea <sebasbad@gmail.com>
2016-05-04 21:24:09 +0200

    Added geo fence class

---
+++ GeoFenceDemo01/GeoFence.h
@@ -0,0 +1,13 @@
+//
+//  GeoFence.h
+//  GeoFenceDemo01
+//
+//  Created by Sebastián Badea on 4/5/16.
+//  Copyright © 2016 Sebastian Badea. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GeoFence : NSObject
+
+@end
---
+++ GeoFenceDemo01/GeoFence.m
@@ -0,0 +1,13 @@
+//
+//  GeoFence.m
+//  GeoFenceDemo01
+//
+//  Created by Sebastián Badea on 4/5/16.
+//  Copyright © 2016 Sebastian Badea. All rights reserved.
+//
+
+#import "GeoFence.h"
+
+@implementation GeoFence
+
+@end
